d34cd5cd93ae98d21bb54cc7ba99a65ff0f0a81c
Timothy Smith <timothy.smith@verizon.com>
2017-01-12 14:02:31 -0500

    Add post for todos

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'meet mom for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'go to market',
-	completed: false
-}, {
-	id: 3,
-	description: 'pick up cassie',
-	completed: true
-}];
+var todos = [];
+var todoNextdId = 1;
+
+
+// add middleware 
+// json request comes in, then express will parse it
+// and access it 
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
 	
@@ -24,14 +22,44 @@ app.get('/', function(req, res) {
 // GET /todos
 
 app.get('/todos', function(req, res) {
-    // converts our array into json
     res.json(todos);
 });
 
 // GET /todos/:id
 
 app.get('/todos/:id', function(req, res) {
-	res.json('asking for todo with id of ' + req.params.id);
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo;
+
+	todos.forEach(function (todo) {
+		if (todoId === todo.id) {
+			matchedTodo = todo;
+		}
+	});
+
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+
+});
+
+
+app.post('/todos', function(req, res) {
+	var body = req.body;
+
+	console.log('description');
+
+	body.id = todoNextdId;
+	todos.push(body);
+	todoNextdId++;
+
+	console.log('dump array');
+	console.log(todos);
+
+	res.json(body);
+
 });
 
 app.listen(PORT, function() {
